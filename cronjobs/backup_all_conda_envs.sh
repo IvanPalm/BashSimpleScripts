550061f3c28954bcf9526d9ec664d5b21ea696ff
Ivan Palmegiani <ivan.palmegiani@gmail.com>
2024-05-05 12:27:09 +0200

    debugged script for run with cron

--- cronjobs/backup_all_conda_envs.sh
+++ cronjobs/backup_all_conda_envs.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 #date (dd.mm.yyyy) 08.02.2022
-#update 12.11.2022
+#update 05.05.2024
 #author Ivan Palmegiani
 #purpose Clones all conda environments in system to yml to a directory in home named condaenvs_[DateOfTheClone]
 
 
 TODAY=$(date "+%Y-%m-%d")
+BACKUP_DIR="/home/ivan/condaenvs-$TODAY"
+
 CONDA_BASE=$(conda info --base)
 CONDA_FUNCTION="etc/profile.d/conda.sh"
 CONDA="$CONDA_BASE/$CONDA_FUNCTION"
-source $CONDA
+. $CONDA
 
-mkdir ~/condaenvs-$TODAY
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir $BACKUP_DIR
+  echo "Creating directory $BACKUP_DIR"
+else
+  echo "Directory $BACKUP_DIR already exists"
+fi
 
-ENVS=$(conda env list | grep '^\w' | cut -d' ' -f1)
-for env in $ENVS; do
-    conda activate $env
-    conda env export > $HOME/condaenvs-$TODAY/$env.yml
-    echo "Exporting $env"
+for env in $(conda env list | grep -v '#' | awk '{print $1}')
+do
+        conda env export -n $env > $BACKUP_DIR/${env}.yml
+        echo "Exporting $env!"
 done
 
-# Add line to cron report file
-NOW=$(date +"%Y-%m-%d %H:%M:%S")
-echo -e Backed up all Conda envs on ${NOW} >> $HOME/cron_reports.txt
+# NOW=$(date +"%Y-%m-%d %H:%M:%S")
+# echo -e Backed up all Conda envs on ${NOW} >> $HOME/cron_reports.txt
