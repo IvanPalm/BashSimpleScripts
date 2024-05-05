a47abe3eb52884616a561eeb408d86d43903296e
Ivan Palmegiani <ivan.palmegiani@gmail.com>
2024-05-05 12:27:40 +0200

    removed echo to cron_report file

--- cronjobs/update_code_ext.sh
+++ cronjobs/update_code_ext.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #date (dd.mm.yyyy) 16.09.2022
-#update 12.11.2022
+#update 05.05.2024
 #author Ivan Palmegiani
 #purpose Update all extensions in VC Code 
 
-NOW=$(date +"%Y-%m-%d %H:%M:%S")
 
 for ext in $(code --list-extensions)
 	do
 		code --force --install-extension $ext
 	done
 
-echo -e Updated VS-Code extensions on ${NOW} >> $HOME/cron_reports.txt
+# NOW=$(date +"%Y-%m-%d %H:%M:%S")
+# echo -e Updated VS-Code extensions on ${NOW} >> $HOME/cron_reports.txt
